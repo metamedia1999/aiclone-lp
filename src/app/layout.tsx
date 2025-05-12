b8acfc3fd203fdf743b0dd64a65e515e70468851
metamedia1999 <genta.oikawa@metamoment.co.jp>
2025-05-12 19:08:23 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,13 @@ export default function RootLayout({
   return (
     <html lang="ja" className={`${notoSansJP.variable} ${inter.variable}`}>
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PSN4B5R9');</script>
+<!-- End Google Tag Manager -->
         {/* ✅ Google Analytics（GA4）スクリプト */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-FRVN29KFZZ"
